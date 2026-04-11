aa9f56dd6d47cd4d07c2019a070d035c51f6a8d1
iamsarb001 <sarbbsandhu555@gmail.com>
2026-04-11 23:20:50 +0530

    basic todo working

--- reactmain/src/App.jsx
+++ reactmain/src/App.jsx
@@ -3,15 +3,31 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const[count,setcount] = useState(0);
+  const[todos,setTodos] = useState([
+  {
+      title : "First Title",
+      desc : "My First new Desc is ..."
+  },
+  {
+      title : "Second Title",
+      desc : "My Second new Desc is ..."
+  },
+  {
+      title : "Third Title",
+      desc : "My Third new Desc is ..."
+  },
+]);
 
-  const Counthandler = () => {
-    setcount(count + 1);
-  }
 
   return (
     <div className='container'>
-       <button onClick={Counthandler}> Counter++ {count} </button>
+      <h2> All Todo List - </h2>
+       {todos?.map((todo) => {
+          return (<div>
+             title is - {todo?.title}
+             Desc is - {todo?.desc}
+            </div> )
+       })}
     </div>
   )
 }
